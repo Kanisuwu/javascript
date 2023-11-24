f0baa7ea7c4dca0818abccbbfed12a7bff17f55f
Kanisuwu <kaniskrasue07042003@gmail.com>
2023-11-24 13:28:46 -0300

    Final steps

--- Seção 10 - Projeto Agenda/src/controllers/contactController.js
+++ Seção 10 - Projeto Agenda/src/controllers/contactController.js
@@ -35,3 +35,34 @@ exports.editIndex = (async (req, res) => {
 
     res.render('contact', { contact });
 });
+
+exports.edit = (async (req, res) => {
+    try {
+        if (!req.params.id) return res.render('404');
+        const contact = new Contact(req.body);
+        await contact.edit(req.params.id);
+        if (contact.errors.length > 0) {
+            req.flash('errors', contact.errors);
+            req.session.save(() => res.redirect('back'));
+            return;
+        }
+        req.flash('success', 'Contact Edited.');
+        req.session.save(() => res.redirect(`/contact/index/${contact.contact._id}`));
+        return;
+    }
+    catch (e) {
+        console.log(e);
+        res.render('404');
+    }
+});
+
+exports.delete = async (req, res) => {
+    if (!req.params.id) return res.render('404');
+
+    const contact = await Contact.delete(req.params.id);
+
+    if (!contact) return res.render('404');
+
+    req.flash('success', 'Contact deleted');
+    req.session.save(() => res.redirect('back'));
+};
--- Seção 10 - Projeto Agenda/src/models/ContactModel.js
+++ Seção 10 - Projeto Agenda/src/models/ContactModel.js
@@ -18,11 +18,27 @@ class Contact {
         this.contact = null;
     }
 
+    // Static Methods
+
     static async searchID(id) {
         if (typeof id !== 'string') return;
         const contact = await ContactModel.findById(id);
         return contact;
     }
+
+    static async searchContacts() {
+        const contacts = await ContactModel.find()
+            .sort({ registeredAt: -1 });
+        return contacts;
+    }
+
+    static async delete(id) {
+        if (typeof id !== 'string') return;
+        const contact = await ContactModel.findByIdAndDelete(id);
+        return contact;
+    }
+
+    // Class Methods
 
     async register() {
         this.validate();
@@ -51,6 +67,12 @@ class Contact {
             email: this.body.email,
             phoneNumber: this.body.phoneNumber,
         };
+    }
+    async edit(id) {
+        if (typeof id !== 'string') return;
+        this.validate();
+        if (this.errors.length > 0) return;
+        this.contact = await ContactModel.findByIdAndUpdate(id, this.body, { new: true });
     }
 }
 
